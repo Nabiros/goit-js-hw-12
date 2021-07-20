2e8f7c73932d35b2c06463c3992f8f269ff283cd
Nazar Khizhniak <77328105+Nabiros@users.noreply.github.com>
2021-07-20 20:44:32 +0300

    Finished HW12

---
+++ src/js/getRefs.js
@@ -0,0 +1,7 @@
+const getRefs = {
+    countryInfo: document.querySelector('.country-info'),
+    countryList: document.querySelector('.country-list'),
+    searchCountry: document.querySelector('#search-box'),
+}
+
+export default getRefs;
